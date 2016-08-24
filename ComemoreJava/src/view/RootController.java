4f901c817149e839b5386c475b19e98d78de5935
julio-iuli <carmo.iuli@gmail.com>
2016-08-23 22:28:41 -0300

    box do endereco julio-win

--- ComemoreJava/src/view/RootController.java
+++ ComemoreJava/src/view/RootController.java
@@ -3,7 +3,9 @@ package view;
 import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 
 public class RootController {
 	
@@ -14,7 +16,17 @@ public class RootController {
 			loader.setLocation(Main.class.getResource("../view/ClienteEdit.fxml"));
 			BorderPane clienteEdit = new BorderPane();
 			clienteEdit = loader.load();
-			Main.getRoot().setCenter(clienteEdit);
+			
+			FXMLLoader loader2 = new FXMLLoader();
+			loader2.setLocation(Main.class.getResource("../view/Endereco.fxml"));
+			BorderPane endereco = new BorderPane();
+			endereco = loader2.load();
+			
+			HBox hbox = new HBox();
+			hbox.setAlignment(Pos.CENTER);
+			hbox.getChildren().addAll(clienteEdit, endereco);
+			
+			Main.getRoot().setCenter(hbox);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
