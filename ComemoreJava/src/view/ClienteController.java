f4cfa1f74d91bb07450364ee63f19811387a972d
julio-iuli <carmo.iuli@gmail.com>
2016-08-26 11:36:18 -0300

    voltando ao que deu certo na experiência

--- ComemoreJava/src/view/ClienteController.java
+++ ComemoreJava/src/view/ClienteController.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 
 public class ClienteController {
 	
-
+	@FXML
+	private void handleSalvar() {
+	
+	}
 
 }
--- ComemoreJava/src/view/EnderecoController.java
+++ ComemoreJava/src/view/EnderecoController.java
@@ -5,6 +5,32 @@ import javafx.scene.control.TextField;
 
 public class EnderecoController {
 	
-
+	@FXML
+	private TextField ufField;
+	@FXML
+	private TextField cidadeField;
+	@FXML
+	private TextField bairroField;
+	@FXML
+	private TextField logradouroField;
+	@FXML
+	private TextField cepField;
+	
+	//private Endereco endereco;
+	
+	@FXML
+	private void initialize(){
+	}
+	
+	@FXML
+	private void showFieldData(){
+		
+		System.out.println(ufField.getText());
+		System.out.println(cidadeField.getText());
+		System.out.println(bairroField.getText());
+		System.out.println(logradouroField.getText());
+		System.out.println(cepField.getText());
+		
+	}
 
 }
